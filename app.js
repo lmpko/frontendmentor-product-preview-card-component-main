5999c6b20151b4082ade87182b14e558ae1c4666
lmpko <lmpkoo@gmail.com>
2023-04-27 20:32:27 +0200

    capitel letter added, error fixed

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import CardComponent from "./src/cardComponent";
+import CardComponent from "./src/CardComponent";
 
 
 const root =ReactDOM.createRoot(document.getElementById("root")) 
